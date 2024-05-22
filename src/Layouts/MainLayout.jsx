f988f9bdb174416ca67e8154245d33898a932fcb
kamruzzamanbayezid <kamruzzamanbayezid07@gmail.com>
2024-05-22 10:01:28 +0600

    added basic project setup

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,10 @@
+
+const MainLayout = () => {
+      return (
+            <div className="max-w-[1170px] mx-auto p-4 lg:p-0">
+                  thid id layout
+            </div>
+      );
+};
+
+export default MainLayout;
